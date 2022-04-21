64eb41cffc0900c9c689822ffdc66530d915dc08
Jeremy Jackson <jerj@coplanar.net>
2022-04-21 16:53:43 -0400

    setup datasource, postgresql dialect, add bean

---
+++ eleph-brain/ejbModule/net/coplanar/beanz/TsCellz.java
@@ -0,0 +1,20 @@
+package net.coplanar.beanz;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class TsCellz
+ */
+@Stateless
+@LocalBean
+public class TsCellz implements TsCellzLocal {
+
+    /**
+     * Default constructor. 
+     */
+    public TsCellz() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
